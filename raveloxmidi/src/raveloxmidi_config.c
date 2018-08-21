cda31cbcbd28a78c40e70f6a54ef273ef590e809
ravelox <github@ravelox.co.uk>
2018-08-20 20:31:42 -0700

    Fixed segfault for missing ALSA file buffer size config item

--- raveloxmidi/src/raveloxmidi_config.c
+++ raveloxmidi/src/raveloxmidi_config.c
@@ -54,6 +54,11 @@ static void config_set_defaults( void )
 	config_add_item("readonly","no");
 	config_add_item("inbound_midi","/dev/sequencer");
 	config_add_item("file_mode", "0640");
+
+#ifdef HAVE_ALSA
+	config_add_item("alsa.input_buffer_size", "4096" );
+#endif
+
 }
 
 static void config_load_file( char *filename )
