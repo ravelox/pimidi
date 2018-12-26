f167be988416be45ebcd06d246fb31927747cdbb
Dave Kelly <github@ravelox.co.uk>
2018-12-26 11:49:33 -0800

    IPv6 Support on master branch

--- python/timing_clock_send.py
+++ python/timing_clock_send.py
@@ -4,9 +4,21 @@ import socket
 import struct
 import time
 
-s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+local_port = 5006
 
-s.connect( ("localhost", 5006 ) )
+if len(sys.argv) == 1:
+        family = socket.AF_INET
+        connect_tuple = ( 'localhost', local_port )
+else:
+        details = socket.getaddrinfo( sys.argv[1], local_port, socket.AF_UNSPEC, socket.SOCK_DGRAM)
+        family = details[0][0]
+        if family == socket.AF_INET6:
+                connect_tuple = ( sys.argv[1], local_port, 0, 0)
+        else:   
+                connect_tuple = ( sys.argv[1], local_port)
+
+s = socket.socket( family, socket.SOCK_DGRAM )
+s.connect( connect_tuple )
 
 # Timing Clock
 bytes = struct.pack( "BB", 0xaa, 0xF8)
