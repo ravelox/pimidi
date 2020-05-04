06e7a0fe431a42568e0bdc947b28699e2cbcda48
ravelox <github@ravelox.co.uk>
2020-05-04 11:44:42 -0700

    Added python script to test real-time MIDI message handling - issue #52

---
+++ python/realtimemsg.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+import socket
+import struct
+import time
+
+local_port = 5006
+
+if len(sys.argv) == 1:
+        family = socket.AF_INET
+        connect_tuple = ( 'localhost', local_port )
+else:
+        details = socket.getaddrinfo( sys.argv[1], local_port, socket.AF_UNSPEC, socket.SOCK_DGRAM)
+        family = details[0][0]
+        if family == socket.AF_INET6:
+                connect_tuple = ( sys.argv[1], local_port, 0, 0)
+        else:   
+                connect_tuple = ( sys.argv[1], local_port)
+
+s = socket.socket( family, socket.SOCK_DGRAM )
+s.connect( connect_tuple )
+
+# Note ON with interleaved Timing Clock
+bytes = struct.pack( "BBBB",0x90, 0xF8, 0x30, 0x70 )
+s.send( bytes )
+
+s.close()
